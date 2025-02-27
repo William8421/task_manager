df361c6d4d7b1b10c0d6cf85a350b1a77f842516
william mallak <william.mallak1@gmail.com>
2025-02-27 18:27:43 +0100

    Specify Node.js version and reinstall dependencies

--- server/dist/helper/gernerateToken.js
+++ server/dist/helper/gernerateToken.js
@@ -3,13 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.verifyToken = exports.generateToken = void 0;
+exports.generateToken = generateToken;
+exports.verifyToken = verifyToken;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const secretKey = process.env.JWT_SECRET || "default_secret";
 function generateToken(payload) {
     return jsonwebtoken_1.default.sign(payload, secretKey, { expiresIn: "24h" });
 }
-exports.generateToken = generateToken;
 function verifyToken(token) {
     try {
         return jsonwebtoken_1.default.verify(token, secretKey);
@@ -18,4 +18,3 @@ function verifyToken(token) {
         return null;
     }
 }
-exports.verifyToken = verifyToken;
